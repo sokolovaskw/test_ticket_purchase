9f7724740e5d96c79ee205f603d75a4d92f6fa58
kuzminova <kuzminova.daria@mail.ru>
2025-09-03 16:47:46 +0300

    тест

--- tests/reportBot.ts
+++ tests/reportBot.ts
@@ -18,6 +18,7 @@ export function getMessage(testInfo: TestInfo) {
     
     console.log('>>>>>> ' + process.env.LINK_GIT);
     console.log('>>>>>> ' + process.env.SECRET_FOR_DASHA);
+    console.log('>>>>>> +++' + process.env);
 
     fetch(`https://api.telegram.org/bot${ process.env.TEST_TELEGRAM_TOKEN }/sendMessage`, {
         method: 'POST',
