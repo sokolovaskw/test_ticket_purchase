464e854d259bfccc518f43e8d51d7cc5d5e6abe3
kuzminova <kuzminova.daria@mail.ru>
2025-09-03 17:25:54 +0300

    ю

--- tests/reportBot.ts
+++ tests/reportBot.ts
@@ -17,10 +17,10 @@ export function getMessage(testInfo: TestInfo) {
                 + `\nВот ссылка -- ${ process.env.LINK_GIT } `;
     
     // console.log('>>>>>> ' + process.env.LINK_GIT);
-    console.log('>>>>>> ' + process.env.SECRET_FOR_DASHA);
+    console.log('>>>>>> ' + process.env.TEST_TELEGRAM_CHAT_ID);
     // console.log('>>>>>> +++' + process.env);
 
-    fetch(`https://api.telegram.org/bot${process.env.TEST_TELEGRAM_TOKEN}/sendMessage`, {
+    fetch(`https://api.telegram.org/bot${ process.env.TEST_TELEGRAM_TOKEN }/sendMessage`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
